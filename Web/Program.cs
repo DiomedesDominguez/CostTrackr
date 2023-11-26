d442937a02af124a2879026f66ff6d08ef09a003
DiomedesDominguez <diomedes.dominguez@gmail.com>
2023-11-25 19:27:32 -0500

    dyhtfgn

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,28 +1,36 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using DNMOFT.CostTrackr.Web.Data;
+using DNMOFT.CostTrackr.Web.Data.Entities.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 //builder.Configuration.AddJsonFile("connectionstrings.json", optional: false, reloadOnChange: true);
 
 // Add services to the container.
-var connectionString = 
+var connectionString =
 
 #if POSTGREDEBUG || POSTGRERELEASE
 builder.Configuration.GetConnectionString("PostgreConnection") ?? 
                         throw new InvalidOperationException("Connection string 'PostgreConnection' not found.");
+                        
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
 #endif
 #if DEBUG || RELEASE
-builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+builder.Configuration.GetConnectionString("DefaultConnection") ??
                         throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-#endif
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+#endif
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<mUser, mRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();

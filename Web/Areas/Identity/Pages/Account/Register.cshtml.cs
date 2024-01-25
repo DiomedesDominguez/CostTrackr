ff4646b8fc255af1ce2e298502f2be1da1a5e156
DiomedesDominguez <diomedes.dominguez@gmail.com>
2024-01-25 19:42:32 -0400

    Update login and register pages

--- Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -45,20 +45,29 @@ namespace DNMOFT.CostTrackR.Web.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+            
+            [Required]
+            [Display(Name = "Nombre")]
+            public string Name { get; set; }
+            
+            [Required]
+            [Display(Name = "Apellido")]
+            public string LastName { get; set; }
+
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "Correo electrónico")]
             public string Email { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Contraseña")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Confirmar contraseña")]
+            [Compare("Password", ErrorMessage = "La contraseña y la confirmación de la contraseña no coinciden.")]
             public string ConfirmPassword { get; set; }
         }
 

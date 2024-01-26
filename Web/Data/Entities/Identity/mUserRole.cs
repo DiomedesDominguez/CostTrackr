8b92793c222b20e52c31e7c369ba5ff264d98afb
DiomedesDominguez <diomedes.dominguez@gmail.com>
2024-01-25 22:50:56 -0400

    Fix non-nullable field error in mUserRole.cs

--- Web/Data/Entities/Identity/mUserRole.cs
+++ Web/Data/Entities/Identity/mUserRole.cs
@@ -15,7 +15,9 @@ namespace DNMOFT.CostTrackR.Web.Data.Entities.Identity
     {
         [MaxLength(15), Column(TypeName = "varchar(15)")]
         [Required, DefaultValue("'127.0.0.1'")]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public string ClientIp { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime Created { get; set; }

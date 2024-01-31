5e9bc7a76fade9a4411f758d63012dc77b31d4ae
DiomedesDominguez <diomedes.dominguez@gmail.com>
2024-01-30 21:05:36 -0400

    Add user name to header

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -2,20 +2,27 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DNMOFT.CostTrackR.Web.Models;
 using Microsoft.AspNetCore.Authorization;
+using DNMOFT.CostTrackR.Web.Data;
 
 namespace DNMOFT.CostTrackR.Web.Controllers;
 [Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
+        _context = context;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
+        var user = _context.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+        if (user != null)
+            ViewData["User"] =string.Concat(user.Name, " ", user.LastName);
+
         return View();
     }
 

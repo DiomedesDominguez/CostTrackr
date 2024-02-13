e568056be95968bd42cca581fd5a41fabd95ca45
DiomedesDominguez <diomedes.dominguez@gmail.com>
2024-02-13 18:50:51 -0400

    Refactor mProductoFamilia class properties

--- src/DataAccess/Entities/App/mProductoFamilia.cs
+++ src/DataAccess/Entities/App/mProductoFamilia.cs
@@ -2,28 +2,14 @@ namespace DNMOFT.CostTrackr.DataAccess.Entities.App;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using DNMOFT.CostTrackr.DataAccess.Entities.Base;
-using DNMOFT.CostTrackr.DataAccess.Enums;
 
-   [Table("mProductosFamilias")]
-    public class mProductoFamilia : BaseEntity
-    {
-        #region Fields and properties
+[Table("mProductosFamilias")]
+public class mProductoFamilia : BaseEntity
+{
+    [MaxLength(200)]
+    public string Descripcion { get; set; } = string.Empty;
 
-        /// <summary>
-        ///     Gets or sets the descripcion.
-        /// </summary>
-        /// <value>The descripcion.</value>
-        [MaxLength(200)]
-        public string Descripcion { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the nombre.
-        /// </summary>
-        /// <value>The nombre.</value>
-        [Required]
-        [MaxLength(30)]
-        public string Nombre { get; set; }
-
-        #endregion
-    }
+    [Required]
+    [MaxLength(30)]
+    public string Nombre { get; set; } = string.Empty;
 }

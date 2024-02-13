68cf7d3b78ffd0ed3f8cfb958e6879d88cbf83c5
DiomedesDominguez <diomedes.dominguez@gmail.com>
2024-02-13 18:22:10 -0400

    Refactor mImpuesto and mOfertaDetail classes

--- src/DataAccess/Entities/App/mImpuesto.cs
+++ src/DataAccess/Entities/App/mImpuesto.cs
@@ -2,36 +2,31 @@ namespace DNMOFT.CostTrackr.DataAccess.Entities.App;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using DNMOFT.CostTrackr.DataAccess.Entities.Base;
-using DNMOFT.CostTrackr.DataAccess.Enums;
 
-    [Table("mImpuestos")]
-    public class mImpuesto : BaseEntity
-    {
-        #region Fields and properties
+[Table("mImpuestos")]
+public class mImpuesto : BaseEntity
+{
+    /// <summary>
+    ///     Gets or sets the descripcion.
+    /// </summary>
+    /// <value>The descripcion.</value>
+    [MaxLength(120)]
+    public string Descripcion { get; set; } = string.Empty;
 
-        /// <summary>
-        ///     Gets or sets the descripcion.
-        /// </summary>
-        /// <value>The descripcion.</value>
-        [MaxLength(120)]
-        public string Descripcion { get; set; }
+    /// <summary>
+    ///     Gets or sets the nombre.
+    /// </summary>
+    /// <value>The nombre.</value>
+    [Required]
+    [MaxLength(30)]
+    public string Nombre { get; set; } = string.Empty;
 
-        /// <summary>
-        ///     Gets or sets the nombre.
-        /// </summary>
-        /// <value>The nombre.</value>
-        [Required]
-        [MaxLength(30)]
-        public string Nombre { get; set; }
+    /// <summary>
+    ///     Gets or sets the tasa.
+    /// </summary>
+    /// <value>The tasa.</value>
+    [Required]
+    [Range(0, 100)]
+    public decimal Tasa { get; set; }
 
-        /// <summary>
-        ///     Gets or sets the tasa.
-        /// </summary>
-        /// <value>The tasa.</value>
-        [Required]
-        [Range(0, 100)]
-        public decimal Tasa { get; set; }
-
-        #endregion
-    }
 }

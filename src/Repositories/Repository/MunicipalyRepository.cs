2a034fba5a9738122119d83605343fe1d8c23d97
DiomedesDominguez <diomedes.dominguez@gmail.com>
2024-02-14 14:55:09 -0400

    Add repository classes for managing Municipality, Pais, and Province entities

--- src/Repositories/Repository/MunicipalyRepository.cs
+++ src/Repositories/Repository/MunicipalyRepository.cs
@@ -4,8 +4,15 @@ using DNMOFT.CostTrackr.Repositories.Base;
 
 namespace DNMOFT.CostTrackr.Repositories;
 
+/// <summary>
+/// Represents a repository for managing Municipality entities.
+/// </summary>
 public class MunicipalyRepository: BaseRepository<mMunicipality>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MunicipalyRepository"/> class.
+    /// </summary>
+    /// <param name="dbContext">The application database context.</param>
     public MunicipalyRepository(ApplicationDbContext dbContext) : base(dbContext)
     {
     }

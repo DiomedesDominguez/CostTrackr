ea378f2d0b09df68d1d06c3a8975994e1a8080c4
DiomedesDominguez <diomedes.dominguez@gmail.com>
2024-03-15 20:56:33 -0400

    Remove unused files and update sidebar

--- src/Web/wwwroot/js/site.js
+++ src/Web/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
     // Call the Usuario internal Web API to set the user's name in _Sidebar.cshtml
-    $.ajax({ url: "usuario" }).then(function (data) {
+    $.ajax({ url: location.origin + "/usuario" }).then(function (data) {
         // Tal vez deba de usar otro tipo de identificador para los campos del sidebar, problema para el Diomedes del futuro
         $(".usuario").text(data.name);
         $(".correo").text(data.email);
